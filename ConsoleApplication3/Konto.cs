7d2ece6d909abe192604e4c9920ce205e1747d3a
jArecki444 <135900@student.uwm.edu.pl>
2016-10-19 15:14:34 +0200

    dodanie klasy Konto

---
+++ ConsoleApplication3/Konto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Konto
+    {
+        public double saldoPoczatkowe;
+        public double saldoKoncowe;
+        public string PrzelewMiedzyKontami(double saldoPoczatkowe, double ile)
+    {
+        saldoKoncowe=ile-saldoPoczatkowe;
+        return "Przelano! zostalo ci: "+ saldoKoncowe;
+    }
+    }
+}
